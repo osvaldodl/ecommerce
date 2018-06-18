1c9e1875b4054c74801b73fd873df0478a1ec58d
osvaldo <dino_osvaldo@hotmail.com>
2018-06-18 13:03:39 -0300

    adding index page

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -4,17 +4,54 @@ namespace Hcode;
 
 use Rain\Tpl;
 
+
 class Page{
 
-	public function __construct(){
+	private $tpl;
+	private $options = array();
+	private $defaults = array(
+		"data"=>array()
+	);
+
+
+	public function __construct($opts = array()){
+
+		$this->options = array_merge($this->defaults, $opts);
+
+		$config = array(
+					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+					"debug"         => false 
+				   );
+		Tpl::configure($config);
+
+		$this->tpl = new Tpl;
+		$this->setData($this->options["data"]);
+		$this->tpl->draw("header");
 
 
 	}
 
 
-	public function __destruct(){
+	public function setTpl($name, $data = array(), $returnHTML = false){
 
-		
+		$this->setData($data);
+		return $this->tpl->draw($name, $returnHTML);
+
+	}
+
+	private function setData($data = array()){
+		foreach ($data as $key => $value) {
+			$this->tpl->assign($key, $value);
+		}
+
+
+	}
+
+	public function __destruct(){
+		$this->tpl->draw("footer");
+
+
 	}
 
 
